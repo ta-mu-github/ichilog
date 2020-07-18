f7350ebcdb43e8ac1c88e6dde684b55bd2bc2a2d
github-kyuuhati <jojo1234dorararara@gmail.com>
2020-07-18 19:12:10 +0900

    今日も1日お疲れ様でした。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_14_092215) do
+ActiveRecord::Schema.define(version: 2020_07_18_083819) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
@@ -32,10 +32,10 @@ ActiveRecord::Schema.define(version: 2020_07_14_092215) do
     t.string "category", null: false
     t.string "content", limit: 10000, default: "", null: false
     t.string "image"
+    t.float "rate"
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.float "rate"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 2020_07_14_092215) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "self_introduction"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
