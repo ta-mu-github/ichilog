78cbbf185d9c0fbbf9443eeca4c11d8cebdf3dd8
github-kyuuhati <jojo1234dorararara@gmail.com>
2020-07-20 18:22:26 +0900

    一旦完成だ・・・

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -3,4 +3,21 @@ $(function() {
 
   $('.Notification').fadeOut(2000);
 
-});
+  $('.Header_right__user_box').click(function(){
+
+    if($('.Header_right__user_box_open').hasClass('open')){
+
+      $('.Header_right__user_box_open').removeClass('open');
+      $('.Header_right__user_box_open').slideUp();
+
+    }else{
+
+      $('.Header_right__user_box_open').addClass('open');  
+      $('.Header_right__user_box_open').slideDown();
+
+    }
+    
+  });
+
+
+  });
