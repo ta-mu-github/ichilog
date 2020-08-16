d063f18c433c405bab898c39ea06c5df3d489611
github-kyuuhati <jojo1234dorararara@gmail.com>
2020-08-16 13:34:22 +0900

    モーダル　背景を暗くする処理追加

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -3,12 +3,14 @@ $(function() {
   /*ログインボタン押下したらモーダルを表示 */
   $('#Login-modal').click(function(){
     // alert("a");
-    $('.Signin_wrapper').fadeIn();
+    $('.Signin').fadeIn();
+    $('.Signin__wrapper').fadeIn();
   });
 
   /*バツボタンを押したらモーダルを閉じる */  
-  $('.Signin_wrapper__container__close_btn').click(function(){
-    $('.Signin_wrapper').fadeOut();
+  $('.Signin__wrapper__container__close_btn').click(function(){
+    $('.Signin').fadeOut();
+    $('.Signin__wrapper').fadeOut();
   });
 
 
