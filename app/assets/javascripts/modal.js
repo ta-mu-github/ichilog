8da90eb58784fd30e7c4f19675c524bb38bf68e7
github-kyuuhati <jojo1234dorararara@gmail.com>
2020-08-22 07:12:20 +0900

    モーダル取りやめに伴うJSのコメントアウト

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -1,17 +1,17 @@
 
 $(function() {
   /*ログインボタン押下したらモーダルを表示 */
-  $('#Login-modal').click(function(){
-    // alert("a");
-    $('.Signin').fadeIn();
-    $('.Signin__wrapper').fadeIn();
-  });
+  // $('#Login-modal').click(function(){
+  //   // alert("a");
+  //   $('.Signin').fadeIn();
+  //   $('.Signin__wrapper').fadeIn();
+  // });
 
-  /*バツボタンを押したらモーダルを閉じる */  
-  $('.Signin__wrapper__container__close_btn').click(function(){
-    $('.Signin').fadeOut();
-    $('.Signin__wrapper').fadeOut();
-  });
+  // /*バツボタンを押したらモーダルを閉じる */  
+  // $('.Signin__wrapper__container__close_btn').click(function(){
+  //   $('.Signin').fadeOut();
+  //   $('.Signin__wrapper').fadeOut();
+  // });
 
 
 });
