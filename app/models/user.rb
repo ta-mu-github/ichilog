abe3a6d02fefc2a4c04af42960eaa5ae9239edf0
github-kyuuhati <jojo1234dorararara@gmail.com>
2020-08-27 10:25:17 +0900

    アソシエーションを組む

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,13 @@ class User < ApplicationRecord
   has_many :comments #コメントテーブルとのアソシエーション
   has_many :likes    #いいねテーブルとのアソシエーション
 
+  #フォロー機能
+  has_many :relationships
+  has_many :followings, through: :relationships, source: :follow
+  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
+  has_many :followers, through: :reverse_of_relationships, source: :user
+
+
   mount_uploader :profile_image, ImageUploader
 
   def self.search(search)
