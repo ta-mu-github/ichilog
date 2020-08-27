3eb5d912782b09053dcd9de270b11fa7d565fe32
github-kyuuhati <jojo1234dorararara@gmail.com>
2020-08-27 10:26:19 +0900

    フォローメソッドを記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,24 @@ class User < ApplicationRecord
   has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverse_of_relationships, source: :user
 
+  #メソッド（モデル内に書く）
+  def follow(other_user)
+    unless self == other_user
+      self.relationships.find_or_create_by(follow_id: other_user.id)
+    end
+  end
+
+  def unfollow(other_user)
+    relationship = self.relationships.find_by(follow_id: other_user.id)
+    relationship.destroy if relationship
+  end
+
+  def following?(other_user)
+    self.followings.include?(other_user)
+  end
+
+
+
 
   mount_uploader :profile_image, ImageUploader
 
